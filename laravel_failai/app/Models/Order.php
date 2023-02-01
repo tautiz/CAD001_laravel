e00447ada791ac1d1486d50971d51f975a827753
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-02-01 09:54:37 +0200

    Adding Order properties annotations

--- laravel_failai/app/Models/Order.php
+++ laravel_failai/app/Models/Order.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $shipping_address_id
+ * @property int $billing_address_id
+ * @property int $payment_id
+ * @property int $status_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class Order extends Model
 {
     use HasFactory;
