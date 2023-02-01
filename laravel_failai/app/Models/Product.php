7e2532449338b04a725f762f967447dba88e6e45
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-02-01 10:31:50 +0200

    Adding missing Product Model

---
+++ laravel_failai/app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Product
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string $description
+ * @property string $image
+ * @property int $category_id
+ * @property string $color
+ * @property string $size
+ * @property int $price
+ * @property int $status_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Product extends Model
+{
+    use HasFactory;
+}
