89fc9096ec4f37fe0849f4b14f5d401c77f5beb8
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-02-14 11:57:30 +0200

    New migration for File system feature

---
+++ laravel_failai/database/migrations/2023_02_14_094619_create_file_data_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('path');
+            $table->string('extension')->nullable();
+            $table->string('size')->nullable();
+            $table->string('url');
+            $table->unsignedBigInteger('model_id')->nullable();
+            $table->string('model_type')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
